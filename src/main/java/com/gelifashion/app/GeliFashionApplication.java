dfc31cfa68d3173c480ee34a3be8aa41efd492bb
Bustami Rey <breymo87@gmail.com>
2026-01-13 09:38:00 -0500

    feat: initial Spring Boot setup and hello endpoint

---
+++ src/main/java/com/gelifashion/app/GeliFashionApplication.java
@@ -0,0 +1,13 @@
+package com.gelifashion.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeliFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeliFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gelifashion/app/GeliFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelifashion.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeliFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
